e0e701c5e962ab3cba6328592fccdc6ece1c176a
eamtc <prog.eamtc@gmail.com>
2020-02-24 20:01:56 +0200

    first commit

---
+++ pr1.java
@@ -0,0 +1,11 @@
+public class pr1 {
+	public static void main(String[] args) {
+		char litera = 'a';
+		if(litera == 'a' ||  litera == 'e' || litera == 'i' || litera == 'o' || litera == 'u' ){
+			System.out.println("Vocala");
+		}
+		else {
+			System.out.println("Consoana");			
+		}
+	}
+}
---
+++ pr1V2.java
@@ -0,0 +1,26 @@
+public class pr1V2 {
+	public static void main(String[] args) {
+		char litera = 'b';
+		switch(litera){
+			case 'a':{
+				
+			}
+			case 'e':{
+			
+			}
+			case 'i':{
+			
+			}
+			case 'o':{
+			
+			}
+			case 'u':{
+				System.out.println("Vocala");
+				break;
+			}
+			default:{
+				System.out.println("Consoana");				
+			}
+		}
+	}
+}
