797d886378259f8f0f0d3015a215ad766890a947
eamtc <prog.eamtc@gmail.com>
2020-02-24 20:16:09 +0200

    second commit

---
+++ pr2.java
@@ -0,0 +1,10 @@
+public class pr2 {
+	public static void main(String[] args) {
+		int i = 0,n = 10,suma = 0;
+		while(i <= n){
+			suma+=i;
+			i++;
+		}
+		System.out.println(suma);
+	}
+}
---
+++ pr2V2.java
@@ -0,0 +1,11 @@
+public class pr2V2 {
+	public static void main(String[] args) {
+		int i = 0,n = 10,suma = 0;
+		do{
+			suma+=i;
+			i++;
+		} while(i <= n);
+
+		System.out.println(suma);
+	}
+}
