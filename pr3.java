e0c92246f2207073eeaf0fe60ba6325140a73048
eamtc <prog.eamtc@gmail.com>
2020-02-24 20:27:48 +0200

    third commit

---
+++ pr3.java
@@ -0,0 +1,9 @@
+public class pr3 {
+	public static void main(String[] args) {
+		int n = 10,produs = 0;
+		for(int i = 0; i <= 10;i++){
+			produs = n * i;
+			System.out.println(n+" * "+i +" = "+produs);
+		}
+	}
+}
